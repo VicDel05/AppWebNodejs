ccfd9b48ea4684d64a2bb72d1cfb8cdf06ebab91
VicDel05 <badelvuctor@gmail.com>
2024-07-02 22:26:56 -0500

    Organizacion de carpetas e instalacion de dependencias

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
-const database = require('./database');
+const session = require('express-session');
+const path = require('path');
+const dotenv = require('dotenv');
+const database = require('./config/database');
 
+// Session
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+
+// Endpoints
 app.get('/', (req, res) =>{
     res.send("Servidor encendido");
-})
+});
+
+// Data Base
+database.connect((err)=> {
+    if (err) {
+        console.error('Error al conectar la base de datos: ', err);
+        return;
+    }
+    console.log('Base de datos conectada');
+});
 
 const port = 3000;
 app.listen(port, () => {
